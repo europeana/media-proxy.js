8279e12181b7f930f3d950574e349e3b1d97679d
Richard Doe <richard.doe@rwdit.net>
2023-05-08 13:06:16 +0100

    chore: reduce line length

--- bin/sonar-project-version.js
+++ bin/sonar-project-version.js
@@ -1,14 +1,15 @@
 // TODO: this duplicates the code from the same file in europeana/portal.js:
 //       extract to a reusable helper script
-import fs from 'fs';
+import fs from 'fs'
 
 const versionSonarProjectProperties = () => {
-  const sonarProjectPropertiesFilePath = new URL('../sonar-project.properties', import.meta.url);
+  const sonarProjectPropertiesFilePath = new URL('../sonar-project.properties', import.meta.url)
 
-  let sonarProjectProperties = fs.readFileSync(sonarProjectPropertiesFilePath, { encoding: 'utf8' });
-  sonarProjectProperties = sonarProjectProperties.replace(/sonar\.projectVersion=.*/, `sonar.projectVersion=${process.env.npm_package_version}`);
+  let sonarProjectProperties = fs.readFileSync(sonarProjectPropertiesFilePath, { encoding: 'utf8' })
+  sonarProjectProperties = sonarProjectProperties
+    .replace(/sonar\.projectVersion=.*/, `sonar.projectVersion=${process.env.npm_package_version}`)
 
-  fs.writeFileSync(sonarProjectPropertiesFilePath, sonarProjectProperties);
-};
+  fs.writeFileSync(sonarProjectPropertiesFilePath, sonarProjectProperties)
+}
 
-versionSonarProjectProperties();
+versionSonarProjectProperties()
