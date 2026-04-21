51cfbb10eceb5bdd445c3bc1d1b1a945fcb750d0
Richard Doe <richard.doe@rwdit.net>
2026-04-21 13:17:57 +0100

    test: spec request/response handling

--- tests/unit/middlewares/web-resource-proxy.spec.js
+++ tests/unit/middlewares/web-resource-proxy.spec.js
@@ -16,6 +16,12 @@ const fixtures = {
     range: 'bytes=11829248-',
     referer: 'https://www.example.org/page.html'
   },
+  resHeadersToDrop: {
+    'set-cookie': 'crumbly'
+  },
+  resHeadersToKeep: {
+    etag: 'v1'
+  },
   webResourceId: 'https://www.example.org/image.jpg',
   webResourceIdWithCharsEncoded: 'https://www.example.org/Te%C5%BEak.jpg',
   webResourceIdWithCharsToEncode: 'https://www.example.org/Težak.jpg'
@@ -24,7 +30,10 @@ const fixtures = {
 describe('@/middlewares/web-resource-proxy.js', () => {
   const fetchResponseMock = {
     body: '',
-    headers: new Headers(),
+    headers: new Headers({
+      ...fixtures.resHeadersToDrop,
+      ...fixtures.resHeadersToKeep
+    }),
     ok: true,
     status: 200
   }
@@ -38,18 +47,25 @@ describe('@/middlewares/web-resource-proxy.js', () => {
   afterAll(sinon.resetBehavior)
 
   describe('middleware', () => {
+    const req = {
+      headers: {
+        ...fixtures.reqHeadersToKeep,
+        ...fixtures.reqHeadersToDrop
+      },
+      params: { datasetId: '123', localId: 'abc', webResourceHash: 'd1299d035beb29c5b3b36e7f7c5c8610' },
+      query: {},
+      set: sinon.spy()
+    }
+    const res = {
+      end: sinon.spy(),
+      locals: { webResourceId: fixtures.webResourceId },
+      redirect: sinon.spy(),
+      set: sinon.spy(),
+      status: sinon.spy()
+    }
     const next = sinon.spy()
-    const req = { headers: {}, params: {}, set: sinon.spy() }
 
-    describe('when response locals has webResourceId', () => {
-      const res = {
-        end: sinon.spy(),
-        locals: { webResourceId: fixtures.webResourceId },
-        redirect: sinon.spy(),
-        set: sinon.spy(),
-        status: sinon.spy()
-      }
-
+    describe('proxying incoming request upstream', () => {
       it('fetches upstream web resource', async () => {
         await webResourceProxyMiddleware(req, res, next)
 
@@ -58,16 +74,7 @@ describe('@/middlewares/web-resource-proxy.js', () => {
       })
 
       describe('proxied request', () => {
-        const req = {
-          headers: {
-            ...fixtures.reqHeadersToKeep,
-            ...fixtures.reqHeadersToDrop
-          },
-          params: {},
-          set: sinon.spy()
-        }
-
-        it('proxies select headers', async () => {
+        it('proxies certain headers', async () => {
           await webResourceProxyMiddleware(req, res, next)
 
           const proxyReqHeaders = global.fetch.getCalls()[0].args[1].headers
@@ -95,40 +102,231 @@ describe('@/middlewares/web-resource-proxy.js', () => {
           expect(proxyReqHeaders.get('user-agent').endsWith(' (https://www.europeana.eu)')).toBe(true)
         })
       })
+    })
 
-      describe('response', () => {
-        describe('custom x-europeana-web-resource header', () => {
-          it('is set to web resource ID', () => {
-            webResourceProxyMiddleware(req, res, next)
+    describe('sending proxied response downstream', () => {
+      describe('when upstream resource is an HTML document', () => {
+        beforeEach(() => {
+          global.fetch.resolves({
+            ...fetchResponseMock,
+            headers: new Headers({
+              ...fetchResponseMock.headers,
+              'content-type': 'text/html'
+            })
+          })
+        })
 
-            expect(res.set.calledWith('x-europeana-web-resource', fixtures.webResourceId)).toBe(true)
+        it('302 redirects to it', async () => {
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(res.redirect.calledWith(302, fixtures.webResourceId)).toBe(true)
+        })
+      })
+
+      describe('when upstream response is an http error', () => {
+        beforeEach(() => {
+          global.fetch.resolves({
+            ...fetchResponseMock,
+            ok: false,
+            status: 404
+          })
+        })
+
+        it('sends it to next', async () => {
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(next.calledWith(sinon.match((err) => err.status === 404))).toBe(true)
+        })
+      })
+
+      describe('when fetch threw an error, e.g. ssl/network issue', () => {
+        beforeEach(() => {
+          global.fetch.rejects(new Error('Network error'))
+        })
+
+        it('sends a 502-status error to next', async () => {
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(next.calledWith(sinon.match((err) => err.status === 502))).toBe(true)
+        })
+      })
+
+      describe('custom x-europeana-web-resource header', () => {
+        it('is set to web resource ID', () => {
+          webResourceProxyMiddleware(req, res, next)
+
+          expect(res.set.calledWith('x-europeana-web-resource', fixtures.webResourceId)).toBe(true)
+        })
+
+        it('encodes characters not permitted in HTTP header values', () => {
+          webResourceProxyMiddleware(req, {
+            ...res,
+            locals: { webResourceId: fixtures.webResourceIdWithCharsToEncode }
+          }, next)
+
+          console.log('res.set.getCalls', JSON.stringify(res.set.getCalls(), null, 2))
+          expect(res.set.calledWith('x-europeana-web-resource', fixtures.webResourceIdWithCharsEncoded)).toBe(true)
+        })
+      })
+
+      it('proxies certain headers', async () => {
+        await webResourceProxyMiddleware(req, res, next)
+
+        for (const name in fixtures.resHeadersToKeep) {
+          expect(res.set.calledWith(name, fixtures.resHeadersToKeep[name])).toBe(true)
+        }
+      })
+
+      it('drops other headers', async () => {
+        await webResourceProxyMiddleware(req, res, next)
+
+        for (const name in fixtures.resHeadersToDrop) {
+          expect(res.set.calledWith(name, fixtures.resHeadersToDrop[name])).toBe(false)
+        }
+      })
+
+      describe('content-type header', () => {
+        it('first, uses upstream content-type if not "application/octet-stream"', async () => {
+          global.fetch.resolves({
+            ...fetchResponseMock,
+            headers: new Headers({
+              ...fetchResponseMock.headers,
+              'content-type': 'image/png'
+            })
           })
 
-          it('encodes characters not permitted in HTTP header values', () => {
-            webResourceProxyMiddleware(req, {
-              ...res,
-              locals: { webResourceId: fixtures.webResourceIdWithCharsToEncode }
-            }, next)
+          await webResourceProxyMiddleware(req, res, next)
 
-            expect(res.set.calledWith('x-europeana-web-resource', fixtures.webResourceIdWithCharsEncoded)).toBe(true)
+          expect(res.set.calledWith('content-type', 'image/png')).toBe(true)
+        })
+
+        it('second, is dervied from upstream content-disposition if present', async () => {
+          global.fetch.resolves({
+            ...fetchResponseMock,
+            headers: new Headers({
+              ...fetchResponseMock.headers,
+              'content-disposition': 'attachment; filename="model.obj"',
+              'content-type': 'application/octet-stream'
+            })
+          })
+
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(res.set.calledWith('content-type', 'model/obj')).toBe(true)
+        })
+
+        it('last, falls back to "application/octet-stream"', async () => {
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(res.set.calledWith('content-type', 'application/octet-stream')).toBe(true)
+        })
+      })
+
+      describe('content-disposition response header', () => {
+        it('defaults to "attachment"', async () => {
+          global.fetch.resolves({
+            ...fetchResponseMock,
+            headers: new Headers({
+              ...fetchResponseMock.headers,
+              'content-type': 'image/jpeg'
+            })
+          })
+
+          await webResourceProxyMiddleware(req, res, next)
+
+          expect(res.set.calledWith(
+            'content-disposition',
+            sinon.match((value) => value.startsWith('attachment; '))
+          )).toBe(true)
+        })
+
+        describe('with disposition=inline in request query', () => {
+          it('is "inline"', async () => {
+            global.fetch.resolves({
+              ...fetchResponseMock,
+              headers: new Headers({
+                ...fetchResponseMock.headers,
+                'content-type': 'image/jpeg'
+              })
+            })
+
+            await webResourceProxyMiddleware({ ...req, query: { disposition: 'inline' } }, res, next)
+
+            expect(res.set.calledWith(
+              'content-disposition',
+              sinon.match((value) => value.startsWith('inline; '))
+            )).toBe(true)
+          })
+        })
+
+        describe('filename', () => {
+          describe('basename', () => {
+            it('is derived from request parameters', async () => {
+              global.fetch.resolves({
+                ...fetchResponseMock,
+                headers: new Headers({
+                  ...fetchResponseMock.headers,
+                  'content-type': 'image/jpeg'
+                })
+              })
+
+              await webResourceProxyMiddleware(req, res, next)
+
+              expect(res.set.calledWith(
+                'content-disposition',
+                sinon.match((value) => value.includes('filename="Europeana.eu-123-abc-d1299d035beb29c5b3b36e7f7c5c8610'))
+              )).toBe(true)
+            })
+          })
+
+          describe('extension', () => {
+            it('first, is taken from upstream content-type header', async () => {
+              global.fetch.resolves({
+                ...fetchResponseMock,
+                headers: new Headers({
+                  ...fetchResponseMock.headers,
+                  'content-type': 'image/jpeg'
+                })
+              })
+
+              await webResourceProxyMiddleware(req, res, next)
+
+              expect(res.set.calledWith(
+                'content-disposition',
+                sinon.match((value) => value.endsWith('.jpeg"'))
+              )).toBe(true)
+            })
+
+            it('second, is dervied from upstream content-disposition header', async () => {
+              global.fetch.resolves({
+                ...fetchResponseMock,
+                headers: new Headers({
+                  ...fetchResponseMock.headers,
+                  'content-disposition': 'attachment; filename="model.obj"'
+                })
+              })
+
+              await webResourceProxyMiddleware(req, res, next)
+
+              expect(res.set.calledWith(
+                'content-disposition',
+                sinon.match((value) => value.endsWith('.obj"'))
+              )).toBe(true)
+            })
+
+            it('finally, falls back to ".bin"', async () => {
+              await webResourceProxyMiddleware(req, res, next)
+
+              expect(res.set.calledWith(
+                'content-disposition',
+                sinon.match((value) => value.endsWith('.bin"'))
+              )).toBe(true)
+            })
           })
         })
       })
     })
   })
-
-  // describe('webResourceProxyOptions', () => {
-  // describe('onError', () => {
-  //   it('calls next', () => {
-  //     const next = sinon.spy()
-  //     const proxyOptions = webResourceProxyOptions(fixtures.webResourceId, next)
-  //     const err = new Error()
-
-  //     proxyOptions.onError(err)
-
-  //     expect(next.calledWith(err)).toBe(true)
-  //   })
-  // })
 
   // describe('onProxyReq', () => {
   //   const next = sinon.spy()
@@ -161,215 +359,4 @@ describe('@/middlewares/web-resource-proxy.js', () => {
   //     expect(req.abort.called).toBe(true)
   //     expect(next.calledWith(sinon.match((err) => err.status === 504))).toBe(true)
   //   })
-
-  //   it('passes error to next middleware', () => {
-  //     const err = new Error()
-  //     const proxyReq = { set: sinon.spy(), setTimeout: sinon.stub().throws(err) }
-
-  //     proxyOptions.onProxyReq(proxyReq, req, res)
-
-  //     expect(next.calledWith(err)).toBe(true)
-  //   })
-  // })
-
-  //   describe('onProxyRes', () => {
-  //     const next = sinon.stub()
-  //     const proxyOptions = webResourceProxyOptions(fixtures.webResourceId, next)
-  //     const proxyRes = { headers: {} }
-  //     const res = { redirect: sinon.spy(), set: sinon.spy() }
-  //     const req = { params: {}, query: {} }
-
-  //     describe('proxy response header filtering', () => {
-  //       it('keeps select headers', () => {
-  //         const proxyRes = {
-  //           headers: {
-  //             etag: 'v1'
-  //           }
-  //         }
-
-  //         proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //         expect(proxyRes.headers.etag).toBe('v1')
-  //       })
-
-  //       it('removes other headers', () => {
-  //         const proxyRes = {
-  //           headers: {
-  //             'set-cookie': 'crumbly'
-  //           }
-  //         }
-
-  //         proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //         expect(proxyRes.headers['set-cookie']).toBeUndefined()
-  //       })
-  //     })
-
-  //     describe('when upstream response is an error', () => {
-  //       const proxyRes = { headers: {}, statusCode: 400 }
-
-  //       it('sends it to next', () => {
-  //         proxyOptions.onProxyRes(proxyRes, null, res)
-
-  //         expect(next.calledWith(sinon.match((err) => err.status === 400))).toBe(true)
-  //       })
-  //     })
-
-  //     describe('when upstream resource is an HTML document', () => {
-  //       const proxyRes = { headers: { 'content-type': 'text/html' } }
-
-  //       it('302 redirects to it', () => {
-  //         proxyOptions.onProxyRes(proxyRes, null, res)
-
-  //         expect(res.redirect.calledWith(302, fixtures.webResourceId)).toBe(true)
-  //       })
-  //     })
-
-  //     describe('when upstream resource is to be proxied', () => {
-  //       const req = {
-  //         params: { datasetId: '123', localId: 'abc', webResourceHash: 'd1299d035beb29c5b3b36e7f7c5c8610' },
-  //         query: {}
-  //       }
-
-  //       describe('content-type header', () => {
-  //         it('first, uses upstream content-type unless "application/octet-stream"', () => {
-  //           const proxyRes = {
-  //             headers: {
-  //               'content-type': 'image/png'
-  //             }
-  //           }
-
-  //           proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //           expect(res.set.calledWith('content-type', 'image/png')).toBe(true)
-  //         })
-
-  //         it('second, is dervied from upstream content-disposition if present', () => {
-  //           const proxyRes = {
-  //             headers: {
-  //               'content-disposition': 'attachment; filename="model.obj"',
-  //               'content-type': 'application/octet-stream'
-  //             }
-  //           }
-
-  //           proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //           expect(res.set.calledWith('content-type', 'model/obj')).toBe(true)
-  //         })
-
-  //         it('last, falls back to "application/octet-stream"', () => {
-  //           const proxyRes = {
-  //             headers: {}
-  //           }
-
-  //           proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //           expect(res.set.calledWith('content-type', 'application/octet-stream')).toBe(true)
-  //         })
-  //       })
-
-  //       describe('content-disposition response header', () => {
-  //         it('defaults to attachment', () => {
-  //           const proxyRes = { headers: { 'content-type': 'image/jpeg' } }
-
-  //           proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //           expect(res.set.calledWith(
-  //             'content-disposition',
-  //             sinon.match((value) => value.startsWith('attachment; '))
-  //           )).toBe(true)
-  //         })
-
-  //         describe('with disposition=inline in request query', () => {
-  //           it('is inline', () => {
-  //             const proxyRes = { headers: { 'content-type': 'image/jpeg' } }
-
-  //             proxyOptions.onProxyRes(proxyRes, { ...req, query: { disposition: 'inline' } }, res)
-
-  //             expect(res.set.calledWith(
-  //               'content-disposition',
-  //               sinon.match((value) => value.startsWith('inline; '))
-  //             )).toBe(true)
-  //           })
-  //         })
-
-  //         describe('filename', () => {
-  //           describe('basename', () => {
-  //             it('is derived from request parameters', () => {
-  //               const proxyRes = { headers: { 'content-type': 'image/jpeg' } }
-
-  //               proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //               expect(res.set.calledWith(
-  //                 'content-disposition',
-  //                 sinon.match((value) => value.includes('filename="Europeana.eu-123-abc-d1299d035beb29c5b3b36e7f7c5c8610'))
-  //               )).toBe(true)
-  //             })
-  //           })
-
-  //           describe('extension', () => {
-  //             it('first, is taken from upstream content-type header', () => {
-  //               const proxyRes = { headers: { 'content-type': 'image/jpeg' } }
-
-  //               proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //               expect(res.set.calledWith(
-  //                 'content-disposition',
-  //                 sinon.match((value) => value.endsWith('.jpeg"'))
-  //               )).toBe(true)
-  //             })
-
-  //             it('second, is dervied from upstream content-disposition header', () => {
-  //               const proxyRes = { headers: { 'content-disposition': 'attachment; filename="model.obj"' } }
-
-  //               proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //               expect(res.set.calledWith(
-  //                 'content-disposition',
-  //                 sinon.match((value) => value.endsWith('.obj"'))
-  //               )).toBe(true)
-  //             })
-
-  //             it('finally, falls back to ".bin"', () => {
-  //               const proxyRes = { headers: {} }
-
-  //               proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //               expect(res.set.calledWith(
-  //                 'content-disposition',
-  //                 sinon.match((value) => value.endsWith('.bin"'))
-  //               )).toBe(true)
-  //             })
-  //           })
-  //         })
-  //       })
-  //     })
-
-  //     it('passes error to next middleware', () => {
-  //       const err = new Error()
-  //       const res = { set: sinon.stub().throws(err) }
-
-  //       proxyOptions.onProxyRes(proxyRes, req, res)
-
-  //       expect(next.calledWith(err)).toBe(true)
-  //     })
-  //   })
-
-  //   describe('pathRewrite', () => {
-  //     it('returns the web resource URL path plus query', () => {
-  //       const webResourceId = 'https://www.example.org/image.jpg?size=large'
-  //       const proxyOptions = webResourceProxyOptions(webResourceId)
-
-  //       expect(proxyOptions.pathRewrite()).toBe('/image.jpg?size=large')
-  //     })
-  //   })
-
-  //   describe('target', () => {
-  //     it('is set to the web resource URL origin', () => {
-  //       const proxyOptions = webResourceProxyOptions(fixtures.webResourceId)
-
-  //       expect(proxyOptions.target).toBe('https://www.example.org')
-  //     })
-  //   })
-  // })
 })
